c88ebe4bcf76cf3a51b128e989503e35567ebb70
Michael A. Gonzalez <genomics.geek.04.22@gmail.com>
2018-05-14 14:49:07 -0400

    Order chromosomes in lexiographic order.  Makes it nicer for admin and bulk creates :)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -21,3 +21,14 @@ from genome import app_settings, utils
 ])
 def test_reformat_chromosome(chromosome, expected):
     assert utils.reformat_chromosome(chromosome) == expected
+
+
+@pytest.mark.parametrize('chromosome, expected', [
+    ('1', 1),
+    ('X', 23),
+    ('Y', 24),
+    ('M', 25),
+    ('MT', 25),
+])
+def test_chromosome_number(chromosome, expected):
+    assert utils.chromosome_number(chromosome) == expected
