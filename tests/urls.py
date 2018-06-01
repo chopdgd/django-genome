86e28f68d4c2c70a1d5c779eeb3346e5cfaa6caa
Michael A. Gonzalez <genomics.geek.04.22@gmail.com>
2018-06-01 09:29:32 -0400

    Removed GraphQL - not neccessary here

--- tests/urls.py
+++ tests/urls.py
@@ -4,11 +4,8 @@ from __future__ import unicode_literals, absolute_import
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from graphene_django.views import GraphQLView
-
 
 urlpatterns = [
     url(r'^', include('genome.urls', namespace='genome')),
     url(r'^admin/', admin.site.urls),
-    url(r'^graphql', GraphQLView.as_view(graphiql=True)),
 ]
